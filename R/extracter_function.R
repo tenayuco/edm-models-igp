adf05088e1d66770c801cba715b69ac4b21ab8c5
Emilio Mora <emiliomora92@gmail.com>
2026-05-13 12:01:21 +0200

    add the theta to the extracter just to have that and now im plooting ing

--- R/extracter_function.R
+++ R/extracter_function.R
@@ -17,6 +17,17 @@ DF_RT <- process_list(data_list = list_treatment_used$r_hat_list)
 DF_RT_SE <- process_list(data_list = list_treatment_used$r_se_list)
 DF_ALPHA <- process_list(data_list = list_treatment_used$alpha_hat_list)
 DF_ALPHA_SE <- process_list(data_list = list_treatment_used$alpha_se_list)
+  
+  ###
+DF_THETA <- process_list(data_list = list_treatment_used$cv_list_sim)
+DF_THETA <- DF_THETA |> 
+  dplyr::select(theta_o, RMSE_o, replicate)
+
+#i can do this cause you inly have one value per replicate 
+DF_THETA <- unique(DF_THETA)
+  
+
+
 
 LONG_FULL_RT<-long_par_formatter(df_par = DF_RT, df_par_se = DF_RT_SE)
 LONG_FULL_ALPHA <- long_par_formatter(df_par=DF_ALPHA, df_par_se = DF_ALPHA_SE)
@@ -27,13 +38,20 @@ LONG_FULL_RT$type <- "r"
 LONG_FULL_ALPHA$type <- "a"
 
 LONG_FULL <- rbind(LONG_FULL_RT, LONG_FULL_ALPHA)
-  
+
+  #now we merge it with the thetas..
+  # 
+LONG_FULL <- dplyr::inner_join(LONG_FULL, DF_THETA, by="replicate")
+
   
 LONG_FULL$numRep <- list_treatment_used$treatment[["num_rep"]]
 LONG_FULL$numSeed <- list_treatment_used$treatment[["num_seed"]]
 LONG_FULL$rpresent <- list_treatment_used$treatment[["rpresent"]]
 LONG_FULL$enem <- list_treatment_used$treatment[["enem"]]
 
+
+
+  
 return(LONG_FULL)
   
 
