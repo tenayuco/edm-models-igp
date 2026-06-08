3b142994a435685c503f86756d388731fbd25b45
Emilio Mora <emiliomora92@gmail.com>
2026-06-08 17:41:17 +0200

    here on the differences test, im gona take the ccm branch

--- R/CCM_igp_functions.R
+++ R/CCM_igp_functions.R
@@ -1,13 +1,13 @@
 multisp_CCM_igp <- function(x, niter){
   
-
+  list_CCM <- list() #created by emilio
   #calculating the maximum E (with E< m-1) where m is the minum step of time series  
   x_count <- x |> 
     dplyr::mutate(contador =1)|> 
     dplyr::group_by(block) |> 
     dplyr::summarise(m = sum(contador))
 
-  maxE <- (min(x_count$m)/2)-1  #following weak whitney theorem 
+  maxE <- (min(x_count$m)/2)  #following weak whitney theorem 
   #maxE <- 4
   print(paste0("maxE", maxE))
   
@@ -21,19 +21,29 @@ multisp_CCM_igp <- function(x, niter){
     Bccm <- x_multi_ccm$Y[-nrow(x_multi_ccm)]
     
     
+
     Emat <- matrix(nrow=maxE-1, ncol=2)
       
+
     colnames(Emat) <- c("A", "B")
-      
+    
     for(E in 2:maxE) {
       Emat[E-1,"A"] <- multispatialCCM::SSR_pred_boot(A=Accm, E=E, predstep=1, tau=1)$rho 
       Emat[E-1,"B"] <- multispatialCCM::SSR_pred_boot(A=Bccm, E=E, predstep=1, tau=1)$rho
     }
-      
-    E_A <- which.max(na.omit(Emat[,1]))+1
+    
+    list_CCM$Emat <- Emat
+
+    E_A <- which.max(na.omit(Emat[,1]))+1 
     E_B <- which.max(na.omit(Emat[,2]))+1
 
     print(paste0("emant", E_A, E_B))
+
+
+    #Check data for nonlinear signal that is not dominated by noise
+#Checks whether predictive ability of processes declines with
+#increasing time distance
+#See manuscript and R code for details
       
     if(length(E_A)>0 & length(E_B)>0){
       signal_A_out <- multispatialCCM::SSR_check_signal(A=Accm, E=E_A, tau=1, predsteplist=1:10)
@@ -50,21 +60,31 @@ multisp_CCM_igp <- function(x, niter){
          summary(lm(signal_B_out$predatout$rho~signal_B_out$predatout$predstep))$coeff[2,1]>=0){
           
         CCM_boot_A <- multispatialCCM::CCM_boot(Accm, Bccm, E_A, tau=1, iterations=niter)
+        CCM_boot_B <- NA
         CCM_significance_test <- multispatialCCM::ccmtest(CCM_boot_A,CCM_boot_A)
         CCM_significance_test[2] <- 1
       }
       if(summary(lm(signal_A_out$predatout$rho~signal_A_out$predatout$predstep))$coeff[2,1]>=0 &
          summary(lm(signal_B_out$predatout$rho~signal_B_out$predatout$predstep))$coeff[2,1]<0){
-        
+        CCM_boot_A <- NA
+
         CCM_boot_B <- multispatialCCM::CCM_boot(Bccm, Accm, E_B, tau=1, iterations=niter)
         CCM_significance_test <- multispatialCCM::ccmtest(CCM_boot_B,CCM_boot_B)
         CCM_significance_test[1] <- 1
       }
       if(summary(lm(signal_A_out$predatout$rho~signal_A_out$predatout$predstep))$coeff[2,1]>=0 &
          summary(lm(signal_B_out$predatout$rho~signal_B_out$predatout$predstep))$coeff[2,1]>=0){
+                CCM_boot_A <- NA
+        CCM_boot_B <- NA
+
         CCM_significance_test <- c(1,1)
       }
     }
+
+    list_CCM$CCM_boot_A <-  CCM_boot_A
+    list_CCM$CCM_boot_B <-  CCM_boot_B
+    list_CCM$CCM_significance_test <-  CCM_significance_test
+
       
     result <- data.frame(a_cause_b=CCM_significance_test[1], # a = species or X, b= pressure or Y
                            b_cause_a=CCM_significance_test[2])
@@ -76,5 +96,43 @@ multisp_CCM_igp <- function(x, niter){
     
   }else{result <- data.frame(X_cause_Y=NA,Y_cause_X=NA)}
   
-return(result)
+  list_CCM$result <- result
+return(list_CCM)
+}
+
+
+
+data_ts_CCM <-  function(data_pred){
+
+TIME_SERIES_ALL <- data_pred |>
+  tidyr::pivot_longer(cols=c(X, Y, R), names_to = "variable", values_to = "individuals")|>
+  ggplot(aes(x = week, y = individuals)) +
+    geom_line(
+      aes(color= as.factor(variable), group = as.factor(interaction(block, variable))),
+      size = 0.5
+    ) +
+    geom_point(aes(color = variable), size = 1) +
+    facet_wrap(~enem, scales = "free_y") +
+    theme_minimal()
+
+  return(TIME_SERIES_ALL)
+}
+
+
+
+plot_rho_CCM <-  function(rho_data){
+
+RHO_PLOT <-rho_data |>
+  ggplot(aes(x = lobs, y = rho)) +
+  geom_line(aes(color= as.factor(variable)),
+      size = 0.5
+    ) +
+   geom_errorbar(aes(ymin=rho- 1.96*rho_sdev,  ymax=rho+ 1.96*rho_sdev, color= as.factor(variable)), width=.2,
+                 position=position_dodge(0.3))+
+  geom_point(aes(color= as.factor(variable)), position=position_dodge(0.3))+
+    facet_wrap(~enem) +
+    theme_minimal()
+  
+  
+  return(RHO_PLOT)
 }
--- analyses/3.ccm_rigal_igp.R
+++ analyses/3.ccm_rigal_igp.R
@@ -1,3 +1,5 @@
+##code for explorations 
+
 DATA_IGP <- readr::read_csv("data/dataIGP_2025.csv")
 
 
@@ -9,30 +11,171 @@ DATA_PRED <-  pred_formatter(DATA_LONG)
 ### we gonna detrend.. so this is hard, and I wonder how it looks. 
 
 
+#here I create a simple function to see how the data changes..
 
 
-##we detrend
-DATA_PRED_std <-DATA_PRED  |> 
- dplyr::mutate(X=detrend_data(X), Y=detrend_data(Y), R=detrend_data(R))
+##here normalized not detrended 
+##normalization
+DATA_PRED_norm <-DATA_PRED  |> 
+  dplyr::group_by(enem) |> 
+  dplyr::mutate(R = R/max(R, na.rm = TRUE), X = X/max(X, na.rm = TRUE), Y = Y/max(Y, na.rm = TRUE))
 
-##we dont deterdn
-DATA_PRED_std <- DATA_PRED
 
-ccm_sp_igp <- data.frame()
+##we detrend using first differences
 
-for (enemies in unique(DATA_PRED_std$enem)){
+DATA_PRED_std<-DATA_PRED |> 
+  dplyr::group_by(block, enem) |> 
+  dplyr::mutate(X= c(NA, diff(X)), Y= c(NA, diff(Y)), R= c(NA, diff(R)))|> 
+  tidyr::drop_na()  
 
-  DATA_PRED_enem <- DATA_PRED_std |> 
-  dplyr::mutate(R = R/max(R, na.rm = TRUE), X = X/max(X, na.rm = TRUE), Y = Y/max(Y, na.rm = TRUE) )|> 
-  dplyr::filter(enem==enemies)
-  ccm_enem <-  multisp_CCM_igp(x = DATA_PRED_enem, niter = 1000)
-  ccm_enem$enem <- enemies
-  ccm_sp_igp <-rbind(ccm_sp_igp, ccm_enem)  
+##normalization
+DATA_PRED_std_norm <-DATA_PRED_std  |> 
+  dplyr::group_by(enem) |> 
+  dplyr::mutate(R = R/max(R, na.rm = TRUE), X = X/max(X, na.rm = TRUE), Y = Y/max(Y, na.rm = TRUE))
+
+
+
+
+
+
+
+
+
+
+typeData = c("norm", "diff_norm", "abs", "diff_abs")
+
+
+for (data in typeData){
+
+if (data== "norm"){chosen_data <- DATA_PRED_norm}
+if (data== "diff_norm"){chosen_data <- DATA_PRED_std_norm}
+if (data== "abs"){chosen_data <- DATA_PRED}
+if (data== "diff_abs"){chosen_data <- DATA_PRED_std}
+
+
+ ##plot tjis data
+
+  ###
+plot_ts <-  data_ts_CCM(chosen_data)
+
+ggsave(
+    plot_ts,
+    filename = paste0("./outputs/CCM/", data, "/", "plot_ts.png"),
+    height = 10,
+    width = 10,
+    create.dir = T
+  )
+
+  
+  
+full_list_ccm <- list()
+
+for (enemies in unique(chosen_data$enem)) {
+  data_enem <- chosen_data |> 
+    dplyr::filter(enem == enemies)
+  print(enemies)
+  list_ccm_enem <- multisp_CCM_igp(x = data_enem, niter = 1000)
+  
+  # Create a nested list with enemy name and results
+  full_list_ccm[[as.character(enemies)]] <- list(
+    enem = enemies,
+    list_ccm_enem = list_ccm_enem
+  )
 }
 
+#condensed results
+#this add a column to the data frame of the enemi, and will bind them 
+ccm_sp_igp <- data.frame()
+
+for (enemies in names(full_list_ccm)){
+  full_list_ccm[[enemies]]$list_ccm_enem$result$enem <- enemies
+  ccm_sp_igp <-rbind(ccm_sp_igp, full_list_ccm[[enemies]]$list_ccm_enem$result)  
+}
+
+  
+dir.create(paste0("./outputs/CCM/", data)) 
+write.csv(ccm_sp_igp, paste0("./outputs/CCM/", data, "/", "ccm_sp_igp.csv"))
+############now lets plot the embedding dimension and the rho
+
+##first the pho
+
+#function to plot the rho for each time series and for each enem 
+
+##extreact the rho values for each enem and put ir into a data frame 
+
+RHO_DATA <- data.frame()
+
+for (enemies in names(full_list_ccm)){
+  print(enemies)
+  
+  if(is.na(full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_A)[[1]]){
+    rho_X <- 0
+    rho_X_sdev <- 0
+    lobsX <- 0
+    print("case1")
+  } else {
+    rho_X <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_A$rho
+    rho_X_sdev <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_A$sdevrho
+    lobsX <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_A$Lobs
+  }
+  
+  if(is.na(full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_B)[[1]]){
+    rho_Y <- 0
+    rho_Y_sdev <- 0
+    lobsY <- 0
+    print("case2")
+  } else {
+    rho_Y <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_B$rho
+    rho_Y_sdev <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_B$sdevrho
+    lobsY <- full_list_ccm[[enemies]]$list_ccm_enem$CCM_boot_B$Lobs
+  }
+  
+  temp_data_X <- data.frame(
+    lobs = lobsX,
+    rho = rho_X,
+    rho_sdev = rho_X_sdev,
+    variable = "X"
+  )
+
+  temp_data_Y <- data.frame(
+    lobs = lobsY,
+    rho = rho_Y,
+    rho_sdev = rho_Y_sdev,
+    variable = "Y"
+  )
+
+  temp_data <- rbind(temp_data_X, temp_data_Y)
+  temp_data$enem <-  enemies
+
+  RHO_DATA<- rbind(RHO_DATA, temp_data)
+}
+
+write.csv(RHO_DATA,  paste0("./outputs/CCM/", data, "/",  "RHO_DATA.csv"))
+
+rho_plot <- plot_rho_CCM(RHO_DATA)
+
+ggsave(
+    rho_plot,
+    filename = paste0("./outputs/CCM/", data, "/", "rho_plot.png"),
+    height = 10,
+    width = 10,
+    create.dir = T
+  )
+
+
+}
+#plot(full_list_ccm$`cc+ma`$list_ccm_enem$CCM_boot_A$rho)
+
+
+
+
+
+
+
+
+#to run rest:
 #so normaly it wull be donde by ene
-#DATA_PRED_std_ex  <- DATA_PRED_std |> 
- # dplyr::filter(enem=="cc+ma")
+#x<- chosen_data |> dplyr::filter(enem=="cc+ma")
 
 
 #this is how the data looks 
