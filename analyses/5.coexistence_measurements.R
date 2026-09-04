33edcafe3b0b7d62f947e023cc36b333972bd77d
Emilio Mora <emiliomora92@gmail.com>
2026-09-04 13:02:06 +0200

    now everyting togerher, and about to do the plots of coexistence

--- analyses/5.coexistence_measurements.R
+++ analyses/5.coexistence_measurements.R
@@ -1,5 +1,9 @@
 #---COEXISTENCE REPRESENTATIONS 
 
+#LOAD the LOTJA VOLTERRA DATA
+
+
+
 # ============================================================================
 # 2. LOAD AND FILTER DATA
 # ============================================================================
@@ -26,6 +30,9 @@ if (type_data == "real.data") {
 
 #=====================================
 
+
+##here we use 2 formats of data
+DATA_PRED <- df_modifier_lv(raw_data = DATA_IGP)
 
 ###coexistence propo
 DATA_BIN <- binary_remove_zeros(DATA_PRED) ##this chnage x and y to 0 and 1 and change 0 to 1 if 1 is next )=(110100 to 111100)
@@ -56,3 +63,35 @@ DATA_SURV_AV <- survival_time_average(DATA_SURV)
 
 
 ###now we gonna put together 1. the omega, 2. the area of coexistence, and 3 the survival time. 
+
+##here you specify wich one wou want 
+full_df <-  read.csv("./outputs/LV_MAP/real.data/absolute/not_normalized/FULL_DF_parameters_numseed_30.csv")
+full_sum <- summarizer_with_variance(df_full = full_df)
+
+##
+COMPLETE_DF <-   dplyr::left_join(full_sum, DATA_AREA, by= "enem")
+COMPLETE_DF <-   dplyr::left_join(COMPLETE_DF, DATA_SURV_AV, by= "enem")
+
+
+##############
+#now some plottings!! Or I can add the IGP and shit 
+
+## just some prepltting
+## SO, we remove the area calculation cause it is equivalente to the first survival measure!! IMPORTANTA RESULT
+
+
+
+
+COMPLETE_DF_LONG <- COMPLETE_DF |> 
+  dplyr::select(enem,grand_mean, total_sd, grand_mean_omega,total_sd_omega , mean_surv, type, varName, sd_surv)|> 
+  tidyr::gather(key= "coexistence_variable", value= "coex_value", grand_mean_omega, mean_surv)|> 
+    tidyr::gather(key= "coexistence_SD", value= "coex_sd", total_sd_omega, sd_surv)
+
+  
+  ## IT DPES SOETHING WRONG
+
+#BUT if I remove thecase that dont correspond togerher, ill sort it out (this is the correpsongin mean and sd, set to na 
+#if not, and then remove the columns with na
+#shoudl wolr
+ # dplyr::mutate(sd_surv = sd_surv |> replace_when(coexistence_variable %in% c("grand_omega_mean", "mean_area"))
+
